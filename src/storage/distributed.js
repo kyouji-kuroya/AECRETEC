c2d327236862635c032c6019870fd5281f6fb977
Kuroya Kyouji <sh.savant@gmail.com>
2024-10-22 23:56:23 +0900

    Added src/storage/distributed.js

---
+++ src/storage/distributed.js
@@ -0,0 +1,27 @@
+class DistributedStorage {
+  constructor() {
+    this.storage = new Map();
+  }
+
+  store(key, value) {
+    if (typeof key !== 'string') {
+      throw new TypeError('Key must be a string');
+    }
+    this.storage.set(key, value);
+  }
+
+  retrieve(key) {
+    if (typeof key !== 'string') {
+      throw new TypeError('Key must be a string');
+    }
+    return this.storage.get(key);
+  }
+
+  demonstrate() {
+    console.log('Distributed Storage System Demo');
+    this.store('file1', 'Content of file 1');
+    console.log('Retrieved:', this.retrieve('file1'));
+  }
+}
+
+module.exports = DistributedStorage;
